4c258caf6af9edb355d2aa38237d99a1d5bb4d5a
Demnu <harrison.joe.collins@gmail.com>
2021-11-22 00:21:44 +1100

    another migration...

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "app", 'build')));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/app/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/app/build', 'index.html'));
 });
 
 const start = async () => {
