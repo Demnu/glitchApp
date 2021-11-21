8dc4c37fdda9e7850d6eadbb7882a46d76cb71a6
Demnu <harrison.joe.collins@gmail.com>
2021-11-22 00:17:40 +1100

    another migration...

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next) {
 app.use('/api/v1/orders', orders);
 app.use('/api/v1/recipes', recipes);
 
-app.use(express.static(path.join(__dirname, "client/app", 'build')));
+app.use(express.static(path.join(__dirname, "app", 'build')));
 
 
 app.get('/', function (req, res) {
